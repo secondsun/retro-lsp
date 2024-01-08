881c55346a6d0ef74ad530f488c71a31b92352b4
Summers Pittman <secondsun@gmail.com>
2024-01-08 15:51:21 -0500

    updates to 1.1.1 common

--- scripts/link_windows.sh
+++ scripts/link_windows.sh
@@ -8,7 +8,7 @@ rm -rf dist/windows
 $JAVA_HOME/bin/jlink \
   --module-path "$JAVA_HOME/jmods;target/classes;target/dependency" \
   --add-modules ALL-MODULE-PATH \
-  --launcher launcher=dev.secondsun.tm4e4lsp/dev.secondsun.tm4e4lsp.Main \
+  --launcher launcher=dev.secondsun.tm4e4lsp/dev.secondsun.retrolsp.Main \
   --output dist/windows \
   --vm=server #\
   #--compress 2 \
